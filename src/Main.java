79aef89e118cbc9055a90300d28ff472beed5866
Bart-PPG <Bartek.nitek@onet.pl>
2024-09-11 11:40:53 +0200

    wpisywanieTablicy

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //tablica musi miec z gory okreslony rozmiar
+        // nie mozna go pozniej zmienic
+        //elemntem tablicy moga byc typy proste i zlozone
+        //tablica 6 elementowa z wartosciami losowymi z zakresu 1 do 100
+        int tabliczaLosowych [] = new int[6];
+        for (int i = 0; i <tabliczaLosowych.length ; i++) {
+            tabliczaLosowych[i] = (int) (Math.random()*100+1);
+        }
+        //wypisywanie elementow z tablicy
+        for (int element:tabliczaLosowych         ) {
+            System.out.print(element+", ");
+        }
     }
 }
